81efd428c2c29153301c0450f26daa5c83b07a77
Mohamed Laribi <mohamedlaribi979@gmail.com>
2026-05-26 23:39:16 +0100

    Add tenant event messaging infrastructure for PaymentService

---
+++ ERPSystem/ERP.PaymentService/Infrastructure/Messaging/TenantEvent/TenantTopics.cs
@@ -0,0 +1,9 @@
+﻿namespace ERP.PaymentService.Infrastructure.Messaging.TenantEvent;
+
+public static class TenantTopics
+{
+    // Tenant lifecycle
+    public const string TenantCreated = "tenant.created";
+    public const string TenantDeleted = "tenant.deleted";
+
+}
