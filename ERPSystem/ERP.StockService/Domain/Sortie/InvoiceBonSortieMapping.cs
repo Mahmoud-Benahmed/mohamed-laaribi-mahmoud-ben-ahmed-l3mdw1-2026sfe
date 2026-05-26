1b3a7618513b295f3993cae6a5c698e0524c9cb1
Mohamed Laribi <mohamedlaribi979@gmail.com>
2026-05-26 23:37:42 +0100

    Add TenantId to InvoiceBonSortieMapping

--- ERPSystem/ERP.StockService/Domain/Sortie/InvoiceBonSortieMapping.cs
+++ ERPSystem/ERP.StockService/Domain/Sortie/InvoiceBonSortieMapping.cs
@@ -3,17 +3,20 @@
 public class InvoiceBonSortieMapping
 {
     public Guid Id { get; private set; }
+    public Guid? TenantId { get; private set; }
+
     public Guid InvoiceId { get; private set; }
     public Guid BonSortieId { get; private set; }
     public DateTime CreatedAt { get; private set; }
 
     private InvoiceBonSortieMapping() { } // EF Core
 
-    public InvoiceBonSortieMapping(Guid invoiceId, Guid bonSortieId)
+    public InvoiceBonSortieMapping(Guid invoiceId, Guid bonSortieId, Guid? tenantId= null)
     {
         Id = Guid.NewGuid();
         InvoiceId = invoiceId;
         BonSortieId = bonSortieId;
+        TenantId = tenantId;
         CreatedAt = DateTime.UtcNow;
     }
 }
