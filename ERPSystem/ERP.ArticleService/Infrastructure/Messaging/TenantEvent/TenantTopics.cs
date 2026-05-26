f94358dd9507259dbf6d3c5c5d478d3a704fa2f9
Mohamed Laribi <mohamedlaribi979@gmail.com>
2026-05-26 23:36:58 +0100

    Add TenantDeleted constant to TenantTopics

--- ERPSystem/ERP.ArticleService/Infrastructure/Messaging/TenantEvent/TenantTopics.cs
+++ ERPSystem/ERP.ArticleService/Infrastructure/Messaging/TenantEvent/TenantTopics.cs
@@ -4,4 +4,6 @@ public static class TenantTopics
 {
     // Tenant lifecycle
     public const string TenantCreated = "tenant.created";
+    public const string TenantDeleted = "tenant.deleted";
+
 }
