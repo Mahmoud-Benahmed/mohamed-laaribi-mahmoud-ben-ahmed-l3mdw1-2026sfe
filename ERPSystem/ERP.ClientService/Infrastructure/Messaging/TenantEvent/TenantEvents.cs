2048852f418817f5438bb5999b557753d080e622
Mohamed Laribi <mohamedlaribi979@gmail.com>
2026-05-26 23:38:50 +0100

    Add tenant event messaging infrastructure for ClientService

---
+++ ERPSystem/ERP.ClientService/Infrastructure/Messaging/TenantEvent/TenantEvents.cs
@@ -0,0 +1,10 @@
+﻿namespace ERP.ClientService.Infrastructure.Messaging;
+public record TenantCreatedEvent(
+    Guid TenantId,
+    string Slug,
+    bool IsActive
+    );
+public record TenantDeletedEvent(
+    Guid TenantId,
+    string Slug
+    );
