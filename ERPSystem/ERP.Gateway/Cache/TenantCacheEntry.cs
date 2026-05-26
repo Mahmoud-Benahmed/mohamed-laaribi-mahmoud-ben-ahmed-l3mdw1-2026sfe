873addba75dcefdfbd1407ade9082c8523e27a6b
Mohamed Laribi <mohamedlaribi979@gmail.com>
2026-05-26 23:37:12 +0100

    Add JsonPropertyName attribute to TenantCacheEntry

--- ERPSystem/ERP.Gateway/Cache/TenantCacheEntry.cs
+++ ERPSystem/ERP.Gateway/Cache/TenantCacheEntry.cs
@@ -1,9 +1,13 @@
 ﻿
+using System.Text.Json.Serialization;
+
 namespace ERP.Gateway.Cache;
 
 public class TenantCacheEntry
 {
-    public Guid TenantId { get; set; } = default!;
+
+    [JsonPropertyName("id")]
+    public Guid TenantId { get; set; }
     public string Slug { get; set; } = default!;
     public bool IsActive { get; set; } = default!;
 }
