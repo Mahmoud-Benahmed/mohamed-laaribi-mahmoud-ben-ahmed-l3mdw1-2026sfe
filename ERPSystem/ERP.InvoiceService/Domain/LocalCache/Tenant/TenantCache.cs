06fd7bea2396628b8696bdd604e0902a21bf8f31
Mohamed Laribi <mohamedlaribi979@gmail.com>
2026-05-26 23:39:08 +0100

    Add TenantCache entity for storing tenant branding and contact info

---
+++ ERPSystem/ERP.InvoiceService/Domain/LocalCache/Tenant/TenantCache.cs
@@ -0,0 +1,42 @@
+﻿using ERP.InvoiceService.Infrastructure.Messaging.Events.TenantEvent;
+
+namespace ERP.InvoiceService.Domain.LocalCache.Tenant;
+
+public class TenantCache
+{
+    public Guid TenantId { get; private set; }
+    public string Slug {get; private set;}
+    public bool IsActive {get; private set;}
+    public string Name {get; private set;}
+    public string Address {get; private set;}
+    public string Email {get; private set;}
+    public string Phone {get; private set;}
+    public string Currency {get; private set;}
+
+    private TenantCache() { }
+    public static TenantCache Create(TenantCreatedEvent evt)
+    {
+        return new TenantCache
+        {
+            TenantId = evt.TenantId,
+            Slug = evt.Slug,
+            IsActive = evt.IsActive,
+            Name = evt.Name,
+            Address = evt.Address,
+            Email = evt.Email,
+            Phone = evt.Phone,
+            Currency = evt.Currency
+        };
+    }
+
+    public void Update(TenantUpdatedEvent evt)
+    {
+        Slug = evt.NewSlug;
+        IsActive = evt.IsActive;
+        Name = evt.Name;
+        Address = evt.Address;
+        Email = evt.Email;
+        Phone = evt.Phone;
+        Currency = evt.Currency;
+    }
+}
