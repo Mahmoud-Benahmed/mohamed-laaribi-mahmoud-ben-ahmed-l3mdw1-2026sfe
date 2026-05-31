0283d67938acab265ba08d66d24df5a1c57a4d32
Mohamed Laribi <mohamedlaribi979@gmail.com>
2026-05-31 04:50:07 +0100

    refactor(auth): remove obsolete TenantTopics constants

--- ERPSystem/ERP.AuthService/Infrastructure/Messaging/TenantTopics.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ERP.AuthService.Infrastructure.Messaging;
-
-public static class TenantTopics
-{
-    // Tenant lifecycle
-    public const string TenantCreated = "tenant.created";
-    public const string TenantDeleted = "tenant.deleted";
-}
