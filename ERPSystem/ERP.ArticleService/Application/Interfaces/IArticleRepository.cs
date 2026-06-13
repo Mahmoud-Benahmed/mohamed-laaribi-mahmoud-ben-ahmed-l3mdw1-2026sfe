c229101d9cce5c10a85868938c0f12fa99282a73
Mohamed Laribi <mohamedlaribi979@gmail.com>
2026-06-13 02:04:55 +0100

    feat(article): add DuplicateExists method to repository interface

--- ERPSystem/ERP.ArticleService/Application/Interfaces/IArticleRepository.cs
+++ ERPSystem/ERP.ArticleService/Application/Interfaces/IArticleRepository.cs
@@ -6,6 +6,7 @@ namespace ERP.ArticleService.Application.Interfaces
     public interface IArticleRepository
     {
         Task AddAsync(Article article);
+        Task<bool> DuplicateExists(string code, Guid? excludeId = null);
         Task<Article?> GetByIdAsync(Guid id);
         Task<Article?> GetByIdDeletedAsync(Guid id);
         Task<Article?> GetByCodeAsync(string code);
